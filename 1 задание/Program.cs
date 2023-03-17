a87629e0a0babfa544d03a0bfb08e72dcf8d117e
Egor <egorefimov078@gmail.com>
2023-03-17 15:38:40 +0300

    1

---
+++ 1 задание/Program.cs
@@ -0,0 +1,17 @@
+﻿int read()
+{
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+    return count;
+}
+Console.Clear();
+Console.WriteLine("Введите несколько чисел (Через пробел): ");
+int result = read();
+Console.WriteLine($"Кол-во элементов > 0: {result}");
